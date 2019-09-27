8134c265373dedc96b79580661c434df089996fb
Niraj <mailme7778@gmail.com>
2019-09-27 16:54:04 +1000

    feat: Added contacts api unit test project and created docker compose for unit test

---
+++ test/Services/Contacts Service/Contacts.API.Test/PingMeControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Contacts.API.Test
+{
+    public class PingMeControllerTest
+    {
+        [Fact]
+        public void PingMeTest()
+        {
+           Assert.Equal(10, 10);
+        }
+    }
+}
